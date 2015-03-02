7e932fcffce412737eb0d11f0f2e2a8604ae9b93
Tom Schaible <tschaible@gmail.com>
2015-03-01 19:14:11 -0500

    finished stylish expressworks exercise

---
+++ expressworks/stylish.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var path = require('path');
+var stylus = require('stylus');
+var app = express();
+
+app.use(express.static(process.argv[3]||path.join(__dirname, 'public')));
+app.use(stylus.middleware(process.argv[3]||path.join(__dirname, 'public')));
+
+
+app.listen(process.argv[2]);
