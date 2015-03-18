a0e5e96d6d4e848e406be03c0db56622c08c0f0b
Tom Schaible <tschaible@gmail.com>
2015-03-18 01:09:09 -0400

    a few bug-clinic exercises for nodeschool.io

---
+++ bug-clinic/triage.js
@@ -0,0 +1,22 @@
+var fs = require("fs");
+
+var peach = function(obj) {
+  console.trace('traced');
+  console.dir(obj);
+};
+
+var bowser = function(callback) {
+  fs.readFile(process.argv[2], {
+    encoding: "utf8"
+  }, callback);
+};
+
+var koopa = function(error, file) {
+  if ( error ) {
+    console.error("error reading file [%s]", error);
+    return;
+  }
+  peach(JSON.parse(file));
+};
+
+bowser(koopa);
